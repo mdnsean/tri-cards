62bc59a21ade06318ffcb567a992bc6931f2e594
Sean <mdnsean@gmail.com>
2015-12-24 21:30:38 -0800

    halfway thru new deck js

--- app/assets/javascripts/dash.js
+++ app/assets/javascripts/dash.js
@@ -1,11 +1,23 @@
 var dash = (function() {
 
     var attachNewDeckHandler = function() {
-
+        var el = getElementById('new-deck-button');
+        el.addEventListener('click', createNewDeck, false);
     };
 
     var start = function() {
-        // initialize methods here
+        attachNewDeckHandler();
+    };
+
+    var createNewDeck = function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/decks/new', true);
+        // display new deck form
+        displayNewDeck();
+    };
+
+    var displayNewDeck = function() {
+        // add html of new deck
     };
 
     return {
