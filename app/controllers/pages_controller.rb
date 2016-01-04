5553f4c7e0c801f348e6cf897008060749d9336a
Sean <mdnsean@gmail.com>
2016-01-03 16:48:38 -0800

    stuck here. hidden this-deck info. still broken add-card's

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
     def dashboard
         @decks = Deck.all
+        @decks.each do |d|
+            instance_variable_set("@deck#{d.id}", d)
+        end
     end
 end
